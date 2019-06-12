8488fb23caa128dbebd1c1ac230be4dee0eb839b
Brian <chesley.brian@gmail.com>
2019-06-12 14:20:08 -0400

    bug fixes to client

--- client.py
+++ client.py
@@ -6,9 +6,10 @@ host = sys.argv[1]
 port = 12345
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((host, port))
 
 while True:
-    response = input() 
+    response = input("Please enter your message! ")
     sock.send(response.encode("utf-8"))
     msg = sock.recv(1000).decode('utf-8')
     print(msg)
